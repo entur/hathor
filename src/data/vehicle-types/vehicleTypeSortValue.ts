77a0288f914a254965ce244e8e063f8779934d7d
Kjetil Midtlie <kjetil.midtlie@gmail.com>
2026-05-27 09:10:50 +0200

    vehicle-types: sort by transportMode (#23)

--- src/data/vehicle-types/vehicleTypeSortValue.ts
+++ src/data/vehicle-types/vehicleTypeSortValue.ts
@@ -1,6 +1,7 @@
 import type { VehicleType } from './vehicleTypeTypes.ts';
 import type { OrderBy } from './useVehicleTypes.ts';
 import { compareWithEmptyLast } from '../../utils/compareWithEmptyLast.ts';
+import { toTransportMode, transportModeSortValue } from '../netex/transportMode.ts';
 
 export const getVehicleTypeSortValue = (item: VehicleType, key: OrderBy): string | number => {
   switch (key) {
@@ -12,6 +13,8 @@ export const getVehicleTypeSortValue = (item: VehicleType, key: OrderBy): string
       return item.length;
     case 'deckPlanName':
       return item.deckPlan?.name?.value || '';
+    case 'transportMode':
+      return transportModeSortValue(toTransportMode(item.transportMode));
     default: {
       const _exhaustive: never = key;
       return _exhaustive;
