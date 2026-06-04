8a9f6de9df331040478fd61ec9122524b331c2cc
Gunnar Gudlaugsson <gunnar.gudlaugsson@egde.no>
2026-06-04 16:18:47 +0200

    Temporary disable of redirect - it doesn't work correctly

--- src/components/auth/ProtectedRoute.tsx
+++ src/components/auth/ProtectedRoute.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { useAuth } from '../../auth';
 import { useConfig } from '../../contexts/configContext.ts';
 import LoginRedirect from '../../auth/LoginRedirect';
-import { useOrganisations } from '../../data/organisations/hooks/useOrganisations.ts';
-import { Navigate } from 'react-router-dom';
+//import { useOrganisations } from '../../data/organisations/hooks/useOrganisations.ts';
+//import { Navigate } from 'react-router-dom';
 
 interface ProtectedRouteProps {
   element: React.ReactElement;
@@ -12,7 +12,7 @@ interface ProtectedRouteProps {
 export const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ element }) => {
   const { isAuthenticated, isLoading } = useAuth();
   const { oidcConfig } = useConfig();
-  const { currentOrganisation } = useOrganisations();
+  //const { currentOrganisation } = useOrganisations();
 
   if (!oidcConfig) {
     return element;
@@ -26,9 +26,10 @@ export const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ element }) => {
     return <LoginRedirect />;
   }
 
-  if (!currentOrganisation) {
-    return <Navigate to="/" replace />;
-  }
+  // Remove this for now - it doesn't work properly.
+  // if (!currentOrganisation) {
+  //   return <Navigate to="/" replace />;
+  // }
 
   return element;
 };
