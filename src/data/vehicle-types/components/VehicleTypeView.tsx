10dc3cf07bf0b2d4f2e8f61a0e1badf3e83ef80f
Gunnar Gudlaugsson <gunnar.gudlaugsson@egde.no>
2026-06-08 13:46:25 +0200

    Add "New vehicle type" button

--- src/data/vehicle-types/components/VehicleTypeView.tsx
+++ src/data/vehicle-types/components/VehicleTypeView.tsx
@@ -4,13 +4,18 @@ import type { VehicleType } from '../types/vehicleTypeTypes.ts';
 import type { OrderBy } from '../hooks/useVehicleTypes.ts';
 import AutosysImportFloatingMenu from '../../vehicle-imports/components/AutosysImportFloatingMenu.tsx';
 import { useUrlFilters } from '../../../hooks/useUrlFilters.ts';
+import NewVehicleTypeFab from './NewVehicleTypeFab.tsx';
 
 export default function VehicleTypeView() {
   const { hasUrlFilters, clearUrlFilters, filterCount } = useUrlFilters();
 
   return (
     <GenericDataViewPage<VehicleType, OrderBy>
-      viewConfig={{ ...vehicleTypeViewConfig, importAction: <AutosysImportFloatingMenu /> }}
+      viewConfig={{
+        ...vehicleTypeViewConfig,
+        importAction: <AutosysImportFloatingMenu />,
+        addAction: <NewVehicleTypeFab />,
+      }}
       urlFilterInfo={{ hasUrlFilters, clearUrlFilters, filterCount }}
     />
   );
